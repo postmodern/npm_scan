d6f0572cd11eed7d0267dc6958236fdb883d82a2
Postmodern <postmodern.mod3@gmail.com>
2022-05-12 03:55:56 -0700

    Add concurrency to the `npm_downloas` executable.

--- src/npm_downloads.cr
+++ src/npm_downloads.cr
@@ -3,36 +3,67 @@ require "./npm_scan/api"
 require "option_parser"
 
 module NPMDownloads
-  period = NPMScan::API::Period::WEEK
+  PERIODS = {
+    "day"   => NPMScan::API::Period::DAY,
+    "week"  => NPMScan::API::Period::WEEK,
+    "month" => NPMScan::API::Period::MONTH
+  }
+
+  period  = NPMScan::API::Period::WEEK
+  workers = 20
 
   OptionParser.parse do |parser|
     parser.banner = "usage: npm_downloads [FILE]"
 
-    parser.on("-d","--last-day","Download counts for the past day") do
-      period = NPMScan::API::Period::DAY
+    parser.on("-p","--period [day|week|month]","Downloads within the the time window. (Default: week)") do |str|
+      period = PERIODS.fetch(str) do
+                 STDERR.puts "error: unknown --period value: #{str}"
+                 exit 1
+               end
     end
 
-    parser.on("-w","--last-week","Download counts for the past week") do
-      period = NPMScan::API::Period::WEEK
-    end
-
-    parser.on("-m","--last-month","Download counts for the past day") do
-      period = NPMScan::API::Period::MONTH
+    parser.on("-w","--workers NUM","The number of concurrent workers.") do |num|
+      workers = num.to_i32
     end
   end
 
-  input = ARGF
-  api   = NPMScan::API.new
+  package_names = Channel(String?).new
 
-  input.each_line do |line|
-    package_name = line.split(/\s+/,2).first
+  spawn do
+    input = ARGF
 
-    begin
-      download_count = api.download_count(package_name, period: period)
+    input.each_line do |line|
+      package_name = line.split(/\s+/,2).first
 
-      puts "#{package_name}: #{download_count}"
-    rescue error : NPMScan::API::HTTPError
-      STDERR.puts "error: #{error.message}"
+      package_names.send(package_name)
     end
+
+    workers.times { package_names.send(nil) }
+  end
+
+  download_counts = Channel({String,Int32}?).new
+
+  workers.times do
+    spawn do
+      api = NPMScan::API.new
+
+      while (package_name = package_names.receive)
+        begin
+          download_count = api.download_count(package_name, period: period)
+
+          download_counts.send({package_name, download_count})
+        rescue error : NPMScan::API::HTTPError
+          STDERR.puts "error: #{error.message}"
+        end
+      end
+
+      download_counts.send(nil)
+    end
+  end
+
+  while (download_count = download_counts.receive)
+    package_name, count = download_count
+
+    puts "#{package_name}: #{count}"
   end
 end
