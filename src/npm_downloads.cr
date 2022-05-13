226c64c497ef1067e5289b99bd5ae4239c4f43ad
Postmodern <postmodern.mod3@gmail.com>
2022-05-12 17:18:46 -0700

    Put CLI code into a CLI class for testability.

--- src/npm_downloads.cr
+++ src/npm_downloads.cr
@@ -3,67 +3,96 @@ require "./npm_scan/api"
 require "option_parser"
 
 module NPMDownloads
-  PERIODS = {
-    "day"   => NPMScan::API::Period::DAY,
-    "week"  => NPMScan::API::Period::WEEK,
-    "month" => NPMScan::API::Period::MONTH
-  }
+  class CLI
 
-  period  = NPMScan::API::Period::WEEK
-  workers = 20
+    PERIODS = {
+      "day"   => NPMScan::API::Period::DAY,
+      "week"  => NPMScan::API::Period::WEEK,
+      "month" => NPMScan::API::Period::MONTH
+    }
 
-  OptionParser.parse do |parser|
-    parser.banner = "usage: npm_downloads [FILE]"
+    getter period : NPMScan::API::Period
 
-    parser.on("-p","--period [day|week|month]","Downloads within the the time window. (Default: week)") do |str|
-      period = PERIODS.fetch(str) do
-                 STDERR.puts "error: unknown --period value: #{str}"
-                 exit 1
-               end
+    getter workers : Int32
+
+    def initialize
+      @period  = NPMScan::API::Period::WEEK
+      @workers = 20
     end
 
-    parser.on("-w","--workers NUM","The number of concurrent workers.") do |num|
-      workers = num.to_i32
-    end
-  end
+    def parse_options : Int32
+      OptionParser.parse do |parser|
+        parser.banner = "usage: npm_downloads [FILE]"
 
-  package_names = Channel(String?).new
+        parser.on("-p","--period [day|week|month]","Downloads within the the time window. (Default: week)") do |str|
+          period = PERIODS.fetch(str) do
+            STDERR.puts "error: unknown --period value: #{str}"
+            exit 1
+          end
+        end
 
-  spawn do
-    input = ARGF
-
-    input.each_line do |line|
-      package_name = line.split(/\s+/,2).first
-
-      package_names.send(package_name)
-    end
-
-    workers.times { package_names.send(nil) }
-  end
-
-  download_counts = Channel({String,Int32}?).new
-
-  workers.times do
-    spawn do
-      api = NPMScan::API.new
-
-      while (package_name = package_names.receive)
-        begin
-          download_count = api.download_count(package_name, period: period)
-
-          download_counts.send({package_name, download_count})
-        rescue error : NPMScan::API::HTTPError
-          STDERR.puts "error: #{error.message}"
+        parser.on("-w","--workers NUM","The number of concurrent workers.") do |num|
+          workers = num.to_i32
         end
       end
 
-      download_counts.send(nil)
+      return 0
     end
-  end
 
-  while (download_count = download_counts.receive)
-    package_name, count = download_count
+    def self.start
+      exit new().run
+    end
 
-    puts "#{package_name}: #{count}"
+    def run : Int32
+      parse_options
+
+      package_names_channel = Channel(String?).new
+
+      spawn do
+        ARGF.each_line do |line|
+          package_name = line.split(/\s+/,2).first
+
+          package_names_channel.send(package_name)
+        end
+
+        workers.times { package_names_channel.send(nil) }
+      end
+
+      download_counts_channel = Channel({String,Int32}?).new
+
+      workers.times do
+        spawn do
+          api = NPMScan::API.new
+
+          while (package_name = package_names_channel.receive)
+            begin
+              download_count = api.download_count(package_name, period: period)
+
+              download_counts_channel.send({package_name, download_count})
+            rescue error : NPMScan::API::HTTPError
+              print_error error.message
+            end
+          end
+
+          download_counts_channel.send(nil)
+        end
+      end
+
+      while (download_count = download_counts_channel.receive)
+        package_name, count = download_count
+
+        puts "#{package_name}: #{count}"
+      end
+
+      return 0
+    end
+
+    @[AlwaysInline]
+    private def print_error(message : String)
+      STDERR.puts "error: #{message}"
+    end
+
   end
 end
+
+NPMDownloads::CLI.start
